ac4400051aa46d6ae8eb7e121b5b2694bd625466
AbdullahSofayan <444101428@student.ksu.edu.sa>
2024-11-08 22:00:52 +0300

    first implementation

--- Main.java
+++ Main.java
@@ -1,42 +1,63 @@
+
+
 class Main{
 
+// Function f(x) = (x^2 + 1) % n
+public static long f(long x, long n) {
+    return (x * x + 1) % n;
+}
+
+// gcd function to compute the greatest common divisor
+public static long gcd(long a, long b) {
+    while (b != 0) {
+        long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Pollard's Rho algorithm to find a non-trivial factor of n
+public static long pollardsRho(long n) {
+    // Initialize x and y
+    long x = 2;
+    long y = 2;
+    long d = 1;
+
+    // Repeat until we find a non-trivial factor
+    while (d == 1 || d == n) {
+        // Calculate the next x and y values
+        x = f(x, n); // x = f(x)
+        y = f(f(y, n), n); // y = f(f(y))
+
+        // Calculate the gcd of the absolute difference between x and y, and n
+        d = gcd(Math.abs(x - y), n); // d = gcd(|x - y|, n)
+
+        // If d is a non-trivial factor, return it
+        if (d != 1 && d != n) {
+            return d;
+        }
+    }
+    return 1; // In case no factor is found (shouldn't happen in theory)
+}
+
 public static void main(String[] args) {
+    // Example number to factor
+    long n = 8051;
 
-    factorization(8051);
+    System.out.println("Factorization of " + n + ":");
 
-}
-
-public static void factorization(int n) {
-    int c =1;
-    int x =2, y=2;
-    int d =1;
-    int f; 
-    int f2;
-    int i =1;
-    while( d!=1 || d!= n) {
-        f = (x*x + c) % n;
-        x = f;
-
-        f = (y*y + c) % n;
-        f2= (f*f + c ) % n;
-        y= f2;
-
-        System.out.println("x"+ i + "is" + x + "and y" + i + "is" + y );
-
-        d = gcd(Math.abs(x) - Math.abs(y), n);  
-
-        
-
-
-    }
-    
-}
-
-    // Recursive Euclidean algorithm
-    public static int gcd(int a, int b) {
-        if (b == 0) 
-            return a;
-        
-        return gcd(b, a % b);
+    // Try to find a factor using Pollard's Rho
+    long factor = pollardsRho(n);
+    if (factor == 1) {
+        System.out.println("No factor found.");
+    } else {
+        System.out.println("A non-trivial factor: " + factor);
+        System.out.println("Other factor: " + (n / factor));
     }
 }
+
+   
+}
+
+
